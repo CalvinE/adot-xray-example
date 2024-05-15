a842e1a71785f332c79ad4071d29bc4cacde025e
CalvinE <calvin.echols@gmail.com>
2024-05-15 10:55:27 -0400

    php app working?

---
+++ verifyservice/vendor/open-telemetry/contrib-aws/tests/Unit/Xray/IdGeneratorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Tests\Aws\Unit\Xray;
+
+use OpenTelemetry\API\Trace\SpanContextValidator;
+use OpenTelemetry\Aws\Xray\IdGenerator;
+use PHPUnit\Framework\TestCase;
+
+class IdGeneratorTest extends TestCase
+{
+    public function testGeneratedTraceIdIsValid()
+    {
+        $this->assertTrue(
+            SpanContextValidator::isValidTraceId(
+                (new IdGenerator())->generateTraceId()
+            )
+        );
+    }
+
+    public function testGeneratedTraceIdIsUnique()
+    {
+        $idGenerator = new IdGenerator();
+
+        $this->assertNotEquals(
+            $idGenerator->generateTraceId(),
+            $idGenerator->generateTraceId()
+        );
+    }
+
+    public function testGeneratedTraceIdTimeStampIsCurrent()
+    {
+        $idGenerator = new IdGenerator();
+        $prevTime = time();
+        $traceId1 = $idGenerator->generateTraceId();
+        $currTime = hexdec(substr($traceId1, 0, 8));
+        $nextTime = time();
+
+        $this->assertGreaterThanOrEqual($prevTime, $currTime);
+        $this->assertLessThanOrEqual($nextTime, $currTime);
+    }
+
+    public function testGeneratedSpanIdIsValid()
+    {
+        $this->assertTrue(
+            SpanContextValidator::isValidSpanId(
+                (new IdGenerator())->generateSpanId()
+            )
+        );
+    }
+}
